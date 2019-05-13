5e021e8fb12282e86c06aaa1de016daf3d981438
Nox <juan.angarita.11@gmail.com>
2019-05-13 17:02:42 -0500

    Ride styles

--- app/views/NewRide.js
+++ app/views/NewRide.js
@@ -24,11 +24,46 @@ class NewRide extends Component {
           backgroundColor={Colors.StatusBar.color}
           barStyle="light-content"
         />
-        <Text>Nuevo Domicilio</Text>
+        <Text style={styles.titleText}>Nuevo Domicilio</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  loginView: {
+    marginTop: 150,
+    backgroundColor: 'transparent',
+    width: 250,
+    height: 400,
+  },
+  loginTitle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: -100
+  },
+  titleText: {
+    color: 'black',
+    fontSize: 30,
+    fontFamily: 'bold',
+  },
+  loginInput: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  footerView: {
+    marginTop: 20,
+    flex: 0.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
 
 const mapStateToProps = state => ({
   auth: state.auth
